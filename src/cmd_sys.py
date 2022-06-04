195f57000ce0d611139e66196c4ee052448eec9e
silvery107 <1079931505@qq.com>
2022-06-04 16:06:43 +0800

    finalize 3 round

--- src/cmd_sys.py
+++ src/cmd_sys.py
@@ -4,8 +4,6 @@
 from navigation_control import AutoNav
 from racetrack_control import Follower
 import rospy
-import cv2
-from utils import FollowingStatus
 
 if __name__ == "__main__":
     rospy.init_node("CMD_Integrated_System")
@@ -18,22 +16,47 @@ if __name__ == "__main__":
     nav.set_initial_pose()
 
     # Start task sequences
-    # TODO add timers for each sub-task
     # Round 1
-    nav.move_goal(5)
-    follower.run(timeout=39)
-    nav.move_goal(2)
-    nav.move_goal(3)
-    nav.move_goal(1)
-    nav.move_goal(0)
+    nav.move_goal(5) #start point of racetrack
+    follower.run({"timeout":15})
+    nav.move_goal(1) #point2
+    nav.move_goal(6) #back point of racetrack
+    follower.run({"timeout":24, "start_once":True})
+    nav.move_goal(2) #point3
+    nav.move_goal(3) #point4
+    # nav.move_goal(7) #search point for aruco
+    nav.move_goal(0) #point1
 
-    follower.refresh_aruco_search()
+
     # Round 2
-    nav.move_goal(5)
-    follower.run(timeout=39)
-    nav.move_goal(2)
-    nav.move_goal(3)
-    nav.move_goal(1)
-    nav.move_goal(0)
+    follower.refresh_aruco_search()
+    nav.move_goal(5) #start point of racetrack
+    follower.run({"timeout":15})
+    nav.move_goal(1) #point2
+    nav.move_goal(6) #back point of racetrack
+    follower.run({"timeout":22, "start_once":True})
+    nav.move_goal(2) #point3
+    nav.move_goal(3) #point4
+    # nav.move_goal(7) #search point for aruco
+    nav.move_goal(0) #point1
+
+
+    # Round 3
+    follower.refresh_aruco_search()
+    nav.move_goal(5) #start point of racetrack
+    follower.run({"timeout":39})
+    follower.stop_seq()
+
+    # TEST
+    # nav.move_goal(1)
+    # nav.move_goal(0) #point1
+    # nav.move_goal(1)
+
+    # follower.rotate_seq(time=0.7, speed=-2)
+    # nav.move_goal(5) #start point of racetrack
+    # nav.move_goal(0) #point1
+    # follower.rotate_seq(time=0.7, speed=-2)
+    # nav.move_goal(5) #start point of racetrack
+    # nav.move_goal(0) #point1
 
     rospy.spin()
